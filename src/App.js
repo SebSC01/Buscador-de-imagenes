272971c3a2989b0a25786e1596a16bfc8c707a6d
SebSC01 <silva.canizalez695@gmail.com>
2022-11-02 11:33:09 -0500

    Se agrega la API
    Se añade los archivos que de usaran para el manejo de la API, con la API_KEY y el Hook 'UseState' para usarlo proximamente

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import Footer from './components/Footer';
+import Api from './components/Api'
 
 function App() {
   return (
     <div>
       <Header />
       <Search />
+      <Api />
       <Footer />
 
     </div>
---
+++ src/components/Api.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './Api.css';
+
+const Api = () => {
+
+    const API_kEY = 'NJvpxIu5A0vfM4aVw2w8bcNxG6oXF8_HBaLslVXL-O4';
+    const URL = 'https://api.unsplash.com/search/photos/?client_id=${API_KEY}';
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Api;
